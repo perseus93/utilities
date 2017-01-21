1f465d27a25dd4bd842f1d61c5cfaeeef86b0859
PSchaap <p_je@hotmail.com>
2017-01-21 03:11:37 +0100

    Finished feature for personal use. May update to a more friendly tool in the future.

--- java/src/utilities/FileSorter.java
+++ java/src/utilities/FileSorter.java
@@ -1,7 +1,20 @@
 package utilities;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  * A utility used for sorting files in a folder.
@@ -18,6 +31,54 @@ public class FileSorter {
 	public static void main(String[] args) {
 		Options options = defineOptions();
 
+		CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine cli = parser.parse(options, args);
+
+			File root = new File(cli.getOptionValue('d'));
+			if (!root.exists()) {
+				System.err.println("Target directory not found.");
+				return;
+			} else if (!root.isDirectory()) {
+				System.err.println("Target directory is not a directory");
+				return;
+			}
+
+			for (File f : root.listFiles()) {
+				if (f.isFile()) {
+					try {
+						BasicFileAttributes attr = Files.readAttributes(f.toPath(), BasicFileAttributes.class);
+
+						FileTime fTime = attr.creationTime();
+						Date d = new Date(fTime.toMillis());
+
+						DateFormat yearFmt = new SimpleDateFormat("yyyy");
+						DateFormat monthFmt = new SimpleDateFormat("MM");
+
+						File subDir = new File(root.getAbsolutePath()	+ File.separatorChar + yearFmt.format(d) + File.separatorChar
+												+ monthFmt.format(d) + File.separatorChar);
+
+						// Create directories if necessary
+						if (!subDir.exists())
+							subDir.mkdirs();
+
+						// Move the file
+						File destination = new File(subDir.getAbsolutePath() + File.separatorChar + f.getName());
+						System.out.println(destination.getAbsolutePath());
+
+						f.renameTo(destination);
+
+					} catch (IOException e) {
+						System.err.printf("[ERROR] Unable to read or move file: %s\n", f.getName());
+					}
+				}
+			}
+
+		} catch (ParseException e) {
+			// Unable to parse arguments, print stacktrace and abort.
+			e.printStackTrace();
+			return;
+		}
 	}
 
 	/**
@@ -28,16 +89,18 @@ public class FileSorter {
 		// Define the CLI options
 		Options options = new Options();
 
-		Option subDirOpt = Option	.builder()
-									.argName("Sub-directory format")
-									.desc("Defines the format to be used for the subdirectories, e.g.: yyyy/mm will result in subdirectories such as 2017/01 for pictures made in january 2017.")
-									.hasArgs()
-									.optionalArg(true)
-									.longOpt("SUB_DIR_FORMAT")
-									.valueSeparator('/')
-									.build();
-		options.addOption(subDirOpt);
+		Option fileOpt = Option	.builder("d")
+								.argName("directory")
+								.desc("The directory that is operated upon")
+								.required()
+								.numberOfArgs(1)
+								.longOpt("DIRECTORY")
+								.build();
+		options.addOption(fileOpt);
+
+		// TODO: Add
 
 		return options;
 	}
+
 }
